33350e0420b92af51c7bad4ec8c36e22e078a124
otpservices99-coder <otpservices99@gmail.com>
2026-06-19 16:16:26 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (!message) return res.status(200).send("ok");
   const chatId = message.chat.id;
   const text = message.text || "Hello!";
-  await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+  await fetch(`https://api.telegram.org/bot8357190104:AAG699Wc_NdJbp6Ie7caG97KQZY4s-C6_OE/sendMessage`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ chat_id: chatId, text: `You said: ${text}` }),
